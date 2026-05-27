f4b13d108cbebd2bff2bc383241c1e2925f00751
CesarSxC <cesarsanchez1204@gmail.com>
2026-05-27 12:01:21 -0500

    visitas externas

--- facial-app-backend/src/main.ts
+++ facial-app-backend/src/main.ts
@@ -23,6 +23,6 @@ async function bootstrap() {
     allowedHeaders: 'Content-Type, Accept, Authorization',
     credentials: true,
   });
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
